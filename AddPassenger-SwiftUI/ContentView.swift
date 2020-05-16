cccadd9c36ce0853c6c898fe4ec8811b3d9e4751
Ali Pishvaee <alipishvaee2@Gmail.com>
2020-05-17 03:00:49 +0430

    Initial Commit

---
+++ AddPassenger-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AddPassenger-SwiftUI
+//
+//  Created by Ali Pishvaee on 5/17/20.
+//  Copyright © 2020 AliPishvaee. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
